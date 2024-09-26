4963b1f6d0a717ef2fad367993cf8b880079ffbc
mayurik-github <161001486+mayurik-github@users.noreply.github.com>
2024-09-26 15:11:47 -0700

    first commit

---
+++ src/main/java/com/example/UserscrudApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.userscrud.entity.Post;
+import com.example.userscrud.entity.User;
+import com.fasterxml.jackson.databind.type.ArrayType;
+
+@SpringBootApplication
+public class UserscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserscrudApplication.class, args);
+		
+		
+		//ConfigurableApplicationContext context = SpringApplication.run(UserscrudApplication.class, args); 
+		
+		//context.getBean(Post.class);
+		
+	}
+
+}
---
+++ src/main/java/com/example/userscrud/service/PostServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.userscrud.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.userscrud.entity.Post;
+import com.example.userscrud.repository.PostRepository;
+
+@Service
+public class PostServiceImpl implements PostService {
+	
+	private PostRepository postRepository;
+	
+	public PostServiceImpl(PostRepository postRepository) {
+		this.postRepository=postRepository;
+	}
+	
+	@Override
+	public Post createPost(Post post) {
+		return postRepository.save(post);
+	}
+
+}
